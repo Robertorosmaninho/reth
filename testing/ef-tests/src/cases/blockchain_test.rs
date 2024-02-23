975eb88a11e1bab52d2387032b1dca57a24306c8
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2024-02-23 21:38:55 +0000

    Forcing all Blockchain Tests

--- testing/ef-tests/src/cases/blockchain_test.rs
+++ testing/ef-tests/src/cases/blockchain_test.rs
@@ -153,46 +153,47 @@ impl Case for BlockchainTestCase {
 ///
 /// The reason should be documented in a comment above the file name(s).
 pub fn should_skip(path: &Path) -> bool {
-    let path_str = path.to_str().expect("Path is not valid UTF-8");
-    let name = path.file_name().unwrap().to_str().unwrap();
-    matches!(
-        name,
-        // funky test with `bigint 0x00` value in json :) not possible to happen on mainnet and require
-        // custom json parser. https://github.com/ethereum/tests/issues/971
-        | "ValueOverflow.json"
-        | "ValueOverflowParis.json"
+    // let path_str = path.to_str().expect("Path is not valid UTF-8");
+    // let name = path.file_name().unwrap().to_str().unwrap();
+    // matches!(
+    //     name,
+    //     // funky test with `bigint 0x00` value in json :) not possible to happen on mainnet and require
+    //     // custom json parser. https://github.com/ethereum/tests/issues/971
+    //     | "ValueOverflow.json"
+    //     | "ValueOverflowParis.json"
 
-        // txbyte is of type 02 and we dont parse tx bytes for this test to fail.
-        | "typeTwoBerlin.json"
+    //     // txbyte is of type 02 and we dont parse tx bytes for this test to fail.
+    //     | "typeTwoBerlin.json"
 
-        // Test checks if nonce overflows. We are handling this correctly but we are not parsing
-        // exception in testsuite There are more nonce overflow tests that are in internal
-        // call/create, and those tests are passing and are enabled.
-        | "CreateTransactionHighNonce.json"
+    //     // Test checks if nonce overflows. We are handling this correctly but we are not parsing
+    //     // exception in testsuite There are more nonce overflow tests that are in internal
+    //     // call/create, and those tests are passing and are enabled.
+    //     | "CreateTransactionHighNonce.json"
 
-        // Test check if gas price overflows, we handle this correctly but does not match tests specific
-        // exception.
-        | "HighGasPrice.json"
-        | "HighGasPriceParis.json"
+    //     // Test check if gas price overflows, we handle this correctly but does not match tests specific
+    //     // exception.
+    //     | "HighGasPrice.json"
+    //     | "HighGasPriceParis.json"
 
-        // Skip test where basefee/accesslist/difficulty is present but it shouldn't be supported in
-        // London/Berlin/TheMerge. https://github.com/ethereum/tests/blob/5b7e1ab3ffaf026d99d20b17bb30f533a2c80c8b/GeneralStateTests/stExample/eip1559.json#L130
-        // It is expected to not execute these tests.
-        | "accessListExample.json"
-        | "basefeeExample.json"
-        | "eip1559.json"
-        | "mergeTest.json"
+    //     // Skip test where basefee/accesslist/difficulty is present but it shouldn't be supported in
+    //     // London/Berlin/TheMerge. https://github.com/ethereum/tests/blob/5b7e1ab3ffaf026d99d20b17bb30f533a2c80c8b/GeneralStateTests/stExample/eip1559.json#L130
+    //     // It is expected to not execute these tests.
+    //     | "accessListExample.json"
+    //     | "basefeeExample.json"
+    //     | "eip1559.json"
+    //     | "mergeTest.json"
 
-        // These tests are passing, but they take a lot of time to execute so we are going to skip them.
-        | "loopExp.json"
-        | "Call50000_sha256.json"
-        | "static_Call50000_sha256.json"
-        | "loopMul.json"
-        | "CALLBlake2f_MaxRounds.json"
-        | "shiftCombinations.json"
-    )
-    // Ignore outdated EOF tests that haven't been updated for Cancun yet.
-    || path_contains(path_str, &["EIPTests", "stEOF"])
+    //     // These tests are passing, but they take a lot of time to execute so we are going to skip them.
+    //     | "loopExp.json"
+    //     | "Call50000_sha256.json"
+    //     | "static_Call50000_sha256.json"
+    //     | "loopMul.json"
+    //     | "CALLBlake2f_MaxRounds.json"
+    //     | "shiftCombinations.json"
+    // )
+    // // Ignore outdated EOF tests that haven't been updated for Cancun yet.
+    // || path_contains(path_str, &["EIPTests", "stEOF"])
+    false
 }
 
 /// `str::contains` but for a path. Takes into account the OS path separator (`/` or `\`).
